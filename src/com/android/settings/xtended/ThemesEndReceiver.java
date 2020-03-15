shivatejapeddi/Themes

Scheduling: Fix receiver early trigger boot issue
Change-Id: Ifcdf23a1da0ceab721b8b907b94ebc5d258b9851

src/com/dirtyunicorns/themes/receivers/ThemesEndReceiver.java
@@ -24,6 +24,7 @@ import static com.dirtyunicorns.themes.Schedule.ScheduleFragment.PREF_THEME_SCHE
 import static com.dirtyunicorns.themes.Schedule.ScheduleFragment.PREF_THEME_SCHEDULED_START_THEME_VALUE;
 import static com.dirtyunicorns.themes.utils.Utils.clearAlarms;
 import static com.dirtyunicorns.themes.utils.Utils.handleBackgrounds;
+import static com.dirtyunicorns.themes.utils.Utils.setEndAlarm;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -56,7 +57,9 @@ public class ThemesEndReceiver extends BroadcastReceiver {
         String scheduledEndThemeValue = mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME_VALUE, null);
         SharedPreferences.Editor sharedPreferencesEditor = mSharedPreferences.edit();
 
-        if (scheduledEndThemeValue != null) {
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()) && scheduledEndThemeValue != null) {
+            setEndAlarm(context);
+        } else if (scheduledEndThemeValue != null) {
             switch (scheduledEndThemeValue) {
                 case "1":
                     handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.PITCH_BLACK, mOverlayManager);
@@ -82,7 +85,7 @@ public class ThemesEndReceiver extends BroadcastReceiver {
                     Toast.makeText(context, context.getString(R.string.theme_type_solarized_dark) + " "
                             + context.getString(R.string.theme_schedule_applied), Toast.LENGTH_SHORT).show();
                     break;
-            }            
+            }
             if (!PreferenceManager.getDefaultSharedPreferences(context)
                     .getBoolean(PREF_THEME_SCHEDULED_REPEAT_DAILY, false)) {
                 sharedPreferencesEditor.putString(PREF_THEME_SCHEDULE, "1");

src/com/dirtyunicorns/themes/receivers/ThemesStartReceiver.java
@@ -19,6 +19,7 @@ package com.dirtyunicorns.themes.receivers;
 import static android.os.UserHandle.USER_SYSTEM;
 import static com.dirtyunicorns.themes.Schedule.ScheduleFragment.PREF_THEME_SCHEDULED_START_THEME_VALUE;
 import static com.dirtyunicorns.themes.utils.Utils.handleBackgrounds;
+import static com.dirtyunicorns.themes.utils.Utils.setStartAlarm;
 
 import android.app.UiModeManager;
 import android.content.BroadcastReceiver;
@@ -41,13 +42,16 @@ public class ThemesStartReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
         mOverlayManager = IOverlayManager.Stub.asInterface(
                 ServiceManager.getService(Context.OVERLAY_SERVICE));
 
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         String scheduledStartThemeValue = mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME_VALUE, null);
 
-        if (scheduledStartThemeValue != null) {
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()) && scheduledStartThemeValue != null) {
+            setStartAlarm(context);
+        } else if (scheduledStartThemeValue != null) {
             switch (scheduledStartThemeValue) {
                 case "1":
                     handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.PITCH_BLACK, mOverlayManager);

jayant-b/packages_apps_Settings

src/com/android/settings/xtended/ThemesEndReceiver.java
@@ -24,6 +24,7 @@ import static com.android.settings.xtended.ScheduleFragment.PREF_THEME_SCHEDULED
 import static com.android.settings.xtended.ScheduleFragment.PREF_THEME_SCHEDULED_START_THEME_VALUE;
 import static com.android.settings.xtended.XThemeUtils.clearAlarms;
 import static com.android.settings.xtended.XThemeUtils.handleBackgrounds;
+import static com.android.settings.xtended.XThemeUtils.setEndAlarm;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -56,7 +57,9 @@ public class ThemesEndReceiver extends BroadcastReceiver {
         String scheduledEndThemeValue = mSharedPreferences.getString(PREF_THEME_SCHEDULED_END_THEME_VALUE, null);
         SharedPreferences.Editor sharedPreferencesEditor = mSharedPreferences.edit();
 
-        if (scheduledEndThemeValue != null) {
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()) && scheduledEndThemeValue != null) {
+            setEndAlarm(context);
+        } else if (scheduledEndThemeValue != null) {
             switch (scheduledEndThemeValue) {
                 case "1":
                     handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.SOLARIZED_DARK, mOverlayManager);

src/com/android/settings/xtended/ThemesStartReceiver.java
@@ -19,6 +19,7 @@ package com.android.settings.xtended;
 import static android.os.UserHandle.USER_SYSTEM;
 import static com.android.settings.xtended.ScheduleFragment.PREF_THEME_SCHEDULED_START_THEME_VALUE;
 import static com.android.settings.xtended.XThemeUtils.handleBackgrounds;
+import static com.android.settings.xtended.XThemeUtils.setStartAlarm;
 
 import android.app.UiModeManager;
 import android.content.BroadcastReceiver;
@@ -41,13 +42,16 @@ public class ThemesStartReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+
         mOverlayManager = IOverlayManager.Stub.asInterface(
                 ServiceManager.getService(Context.OVERLAY_SERVICE));
 
         mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         String scheduledStartThemeValue = mSharedPreferences.getString(PREF_THEME_SCHEDULED_START_THEME_VALUE, null);
 
-        if (scheduledStartThemeValue != null) {
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction()) && scheduledStartThemeValue != null) {
+            setStartAlarm(context);
+        } else if (scheduledStartThemeValue != null) {
             switch (scheduledStartThemeValue) {
                 case "1":
                     handleBackgrounds(false, context, UiModeManager.MODE_NIGHT_NO, ThemesUtils.SOLARIZED_DARK, mOverlayManager);